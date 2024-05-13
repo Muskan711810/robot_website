ab03a19cc04480e479cf9ba4208a5a6e5ebed452
Muskaan Shaikh <ymuskaanshaikh0440@gmail.com>
2024-05-13 19:40:24 +0530

    added components and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import './App.css'
+import Home from './components/Home/Home'
+import Services from './components/Services/Services'
+import Acadmics from './components/Acadmics/Acadmics'
+import Training from './components/Training/Training'
+import Plode from './components/Plode/Plode'
+import AboutUs from './components/AboutUs/AboutUs'
 
 const App = () => {
   return (
     <>
-      
+      <h1>Robot Website</h1>
+      <Home />
+      <Services />
+      <Acadmics />
+      <Training />
+      <Plode />
+      <AboutUs />
     </>
   )
 }
