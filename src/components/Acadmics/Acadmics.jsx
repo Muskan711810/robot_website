a3e4f4728c36f4819c12f067116619d8a41db8c9
Muskaan Shaikh <ymuskaanshaikh0440@gmail.com>
2024-05-15 15:57:01 +0530

    change some css for each content and image

--- src/components/Acadmics/Acadmics.jsx
+++ src/components/Acadmics/Acadmics.jsx
@@ -4,11 +4,60 @@ import './Acadmics.css'
 
 const Acadmics = () => {
   return (
-    <div className="horizontal-line">
+    <div>
   <h2 className='a1'>Early STEM</h2>
+  <h2 className='g1'><span className='g11'>UKG- Grade 1</span>
+  <span className='p1'>
+  <span className='ps1'>1) Creative Exploration </span> 
+  <span className='ps2'>2)Early math's & science<span/> <span className='ps4'>3)LEGO Machines </span>
+  <span className='ps3'>4) Sequencing</span>
+</span></span>
+</h2>
   <h2 className='a2'>Primary</h2>
+  <h2 className='gg1'><span className='g11'>Grade 2-5</span>
+  <span className='p1'>
+  <span className='ps1'>1) Basic programming skills </span> 
+  <span className='ps2'>2)Explanation & design solutions  </span>
+  <span className='ps3'>3)  DIY Electronics </span>
+  <span className='ps5'>4)  Computational thinking </span>
+  <span className='ps3'>5)  Constructivism  </span>
+  <span className='ps3'>6)  Real Life STEM Focus  </span>
+  <span className='ps3'>7)  Basic mechanical principles </span>
+  <span className='ps3'>8)  Simple Machines  </span>
+  <span className='ps3'>9)  Artificial Intelligence  </span><br/>
+  <span className='ps3'>10)  Humanoid Robot </span>
+</span>
+</h2>
   <h2 className='a3'>Middle School</h2>
+  <h2 className='g1'><span className='g11'>Grade 6-9</span>
+  <span className='p1'>
+  <span className='ps1'>1) Virtual Reality </span> 
+  <span className='ps2'>2)Computational  thinking  </span>
+  <span className='ps3'>3)  Problem Solving skills  </span>
+  <span className='ps5'>4)  Input & output  devices  </span><br/>
+  <span className='ps3'>5)  Robotics & Coding </span>
+  <span className='ps3'>6) DIY Electronics & IoT  </span>
+  <span className='ps3'>7) 3D Designing  </span>
+  <span className='ps3'>8)  Design Engineering  </span>
+  <span className='ps3'>9)  Humanoid Robot </span>
+</span>
+</h2>
   <h2 className='a4'>Secondary</h2>
+  <h2 className='g1'><span className='g11'>Grade 10-12</span>
+  <span className='p1'>
+  <span className='ps1'>1)Artifial Intelligence </span> 
+  <span className='ps2'>2)Python Programming  </span>
+  <span className='ps3'>3) Machine learning </span>
+  <span className='ps5'>4)  AI based Self  driving cars  </span><br/>
+  <span className='ps3'>5)  3D Designing  </span>
+  <span className='ps3'>6)  Internet of things   </span>
+  <span className='ps3'>7) Drone Tech</span>
+  <span className='ps3'>8)  AI based Humanoid   Robots  </span>
+</span>
+</h2>
+<div>
+        <p className='contact1'>therobotedcompany@gmail.com  <span className='contact2'>+917780484363,8919455711</span></p>
+      </div>
 </div>
 
   );
